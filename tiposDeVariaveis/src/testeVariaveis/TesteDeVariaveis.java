500073e76c06b413c0085377ca712bb4cc12af4a
RochaJoaoDev <rochajoao.dev@gmail.com>
2024-06-10 06:32:35 -0300

    Testes de tipos de variaveis

---
+++ tiposDeVariaveis/src/testeVariaveis/TesteDeVariaveis.java
@@ -0,0 +1,25 @@
+package testeVariaveis;
+
+public class TesteDeVariaveis {
+
+	public static void main (String[]args) {
+	
+	boolean logica; 
+	boolean logica2;
+	
+	int numero;
+	int numero2;
+	
+	int maiorIdade = 18;
+	int idadeIdoso = 60;
+	int idadeAdolescente = 15;
+	int idadeCrianca = 10;
+	String CPF = "078.111.161-70";
+	
+	System.out.println("Maior idade é: " + maiorIdade);
+	System.out.println("Idade do idoso é: " + idadeIdoso);
+	System.out.println("Idade da criança é:" + idadeCrianca);
+	System.out.println("Seu CPF é: " + CPF);
+	
+	}
+}
---
+++ tiposDeVariaveis/src/tiposDeVariaveis/TiposDeVariaveis.java
@@ -0,0 +1,13 @@
+package tiposDeVariaveis;
+
+public class TiposDeVariaveis {
+
+	public int idade;
+	public String nome;
+	public String sobreNome;
+	public byte bite;
+	public short Short;
+	public boolean sim;
+	 
+	
+}
